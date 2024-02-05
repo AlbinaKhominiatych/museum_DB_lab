613a1e6487763c3a9f5a589994ddeed861db623e
Sergiy <shkum79@ukr.net>
2024-02-05 23:36:20 +0100

    Completed method view_exhibit_info

--- museum_app.py
+++ museum_app.py
@@ -2,6 +2,7 @@ import redis
 import json
 import datetime
 
+
 class MuseumApp:
     def __init__(self, redis_host='localhost', redis_port=6379, redis_db=2):
         self.redis = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
@@ -20,7 +21,13 @@ class MuseumApp:
 
     def view_exhibit_info(self, exhibit_id):
         # Логіка перегляду інформації про експонат
-        ...
+        if self.redis.hexists('exhibits', exhibit_id):
+            return json.loads(self.redis.hget('exhibits', exhibit_id))
+        else:
+            # ни чего не возвращаем, что бы не было ошибка при
+            # проверке возвращаемых данных (if view_exhibit_info(exhibit_id):)
+            # будет возвращен None, значит экспонат не найден
+            print(f'Exhibit with id <{exhibit_id}> not found')
 
     def view_all_exhibits(self):
         # Логіка перегляду всіх експонатів
