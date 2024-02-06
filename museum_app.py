a93ed064d23ea9fb5dacb8dd412411b35214b78b
dl-bk <BahtX_hn46@student.itstep.org>
2024-02-06 21:45:22 +0100

    realisation of ciew_all_exhibits method

--- museum_app.py
+++ museum_app.py
@@ -24,7 +24,9 @@ class MuseumApp:
 
     def view_all_exhibits(self):
         # Логіка перегляду всіх експонатів
-        ...
+        all_exhibits = self.redis.hgetall('exhibits')
+        for key, value in all_exhibits.items():
+            print(f"{key}: {value}")
 
     def view_related_people(self, exhibit_id):
         # Логіка перегляду людей, пов'язаних з експонатом
